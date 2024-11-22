f02481d69c0cd997c3d4abd4042b026acca59a9b
leejin_rho <tbdpapdl@gmail.com>
2024-11-22 10:39:54 +0900

    init: turborepo+react 초기세팅(DURI-174)

---
+++ packages/eslint-config/react.js
@@ -0,0 +1,30 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'turbo',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'turbo/no-undeclared-env-vars': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
