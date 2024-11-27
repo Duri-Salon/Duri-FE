b4fcc279da75a238c4a3ad2518fbc645a00e8562
chanbyeol <cksquf98@naver.com>
2024-11-27 12:56:19 +0900

    feat: payment page 초기 구현(DURI-121)

--- apps/duri/src/App.tsx
+++ apps/duri/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 
 import { globalStyle } from '@duri-fe/ui';
 import { Global } from '@emotion/react';
 
 import Home from '@pages/Home';
+import PaymentPage from '@pages/PaymentPage';
+import { FailPage } from '@pages/PaymentPage/Fail';
+import { SuccessPage } from '@pages/PaymentPage/Success';
 
 
 function App() {
@@ -13,9 +16,11 @@ function App() {
       <Global styles={globalStyle} />
       <Home />
       <div>하이</div>
-      {/* <Routes>
-        <Route path="/payment" element={<Payment />} />
-      </Routes> */}
+      <Routes>
+        <Route path="/payment/:quotationId" element={<PaymentPage />} />
+        <Route path="/payment/success" element={<SuccessPage />} />
+        <Route path="/payment/fail" element={<FailPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
