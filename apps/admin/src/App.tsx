13f2d6273918de99ba053cc592b800ad7ea201bb
chanbyeol <cksquf98@naver.com>
2024-12-19 17:07:51 +0900

    fix: admin 배포 에러 수정 시도, 위치 정보 로컬스토리지에 저장하고 변경 감지하도록 코드 추가

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,20 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { globalStyle } from '@duri-fe/ui';
 import { Global } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Home from '@pages/Home';
 
 import Login from './pages/Login';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Global styles={globalStyle} />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Global styles={globalStyle} />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
